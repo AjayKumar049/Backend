ead0b5e974e0a831d73abb71d7cd98a84f3433df
Ajaykumar Wade <92783730+AjayKumar049@users.noreply.github.com>
2025-04-24 16:03:44 +0530

    Add files via upload

---
+++ Backend/BillingSystem/src/main/java/com/example/BillingSystem/service/AuthenticationImpl.java
@@ -0,0 +1,40 @@
+package com.example.BillingSystem.service;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.example.BillingSystem.exception.BillingSystemAlreadyExist;
+import com.example.BillingSystem.exception.BillingSystemInternalException;
+import com.example.BillingSystem.model.User;
+import com.example.BillingSystem.repository.AuthenticationRepository;
+
+@Service
+public class AuthenticationImpl implements AuthenticationService{
+	
+	private final AuthenticationRepository authenticationRepository;
+
+	public AuthenticationImpl(AuthenticationRepository authenticationRepository) {
+		super();
+		this.authenticationRepository = authenticationRepository;
+	}
+	
+	@Override
+    public User SignUp(User user) {
+        try {
+            // Check if the email already exists
+            if (authenticationRepository.existsByEmail(user.getEmail())) {
+                throw new BillingSystemAlreadyExist("Email already exists");
+            }
+             // Save the customer to the database
+            int save = authenticationRepository.save(user);
+            if (save == 0) {
+                throw new BillingSystemInternalException("Failed to save customer due to internal DB error");
+            }
+
+            return user;
+
+        } catch (DataAccessException e) {
+            throw new BillingSystemInternalException("Database error while saving customer: " + e.getMessage());
+        }
+
+	}}
---
+++ Backend/BillingSystem/src/main/java/com/example/BillingSystem/service/AuthenticationService.java
@@ -0,0 +1,12 @@
+package com.example.BillingSystem.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.BillingSystem.model.User;
+
+@Service
+public interface AuthenticationService {
+	User SignUp(User user);
+	
+
+}
