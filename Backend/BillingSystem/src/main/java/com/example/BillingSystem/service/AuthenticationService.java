2d4789a6896aa2ef9b117fe959c3ff9d0649a345
Ajaykumar Wade <92783730+AjayKumar049@users.noreply.github.com>
2025-04-29 13:59:10 +0530

    Update AuthenticationService.java

--- Backend/BillingSystem/src/main/java/com/example/BillingSystem/service/AuthenticationService.java
+++ Backend/BillingSystem/src/main/java/com/example/BillingSystem/service/AuthenticationService.java
@@ -3,11 +3,11 @@ package com.example.BillingSystem.service;
 import org.springframework.stereotype.Service;
 
 import com.example.BillingSystem.model.User;
+import com.example.BillingSystem.dto.SignupDto;
 
 @Service
 public interface AuthenticationService {
 	User SignUp(User user);
-	User Signin(User user);
-	
+	SignupDto Signin(SignupDto signupDto);
 
 }
