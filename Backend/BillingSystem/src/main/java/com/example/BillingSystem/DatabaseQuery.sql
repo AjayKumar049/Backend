3473e6037a2502a7bea8b4cd16f462902e3dd514
Ajaykumar Wade <92783730+AjayKumar049@users.noreply.github.com>
2025-04-30 16:29:20 +0530

    Update DatabaseQuery.sql

--- Backend/BillingSystem/src/main/java/com/example/BillingSystem/DatabaseQuery.sql
+++ Backend/BillingSystem/src/main/java/com/example/BillingSystem/DatabaseQuery.sql
@@ -34,3 +34,83 @@ CREATE TABLE resetpassword (
     new_password VARCHAR(250),
     FOREIGN KEY (signup_id) REFERENCES signup(signup_id)
 );
+
+Customer, Item, Estimate, Invoice tables query mentioned below
+CREATE TABLE customer (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(20),  -- Business or Individual
+    salutation VARCHAR(10),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    company_name VARCHAR(100),
+    email VARCHAR(100),
+    gstin VARCHAR(15),
+    phone VARCHAR(15)
+);
+
+CREATE TABLE shipping_address (
+    id SERIAL PRIMARY KEY,
+    customer_id INTEGER REFERENCES customer(id),
+    attention VARCHAR(100),
+    country VARCHAR(50),
+    address TEXT,
+    city VARCHAR(50),
+    district VARCHAR(50),
+    state VARCHAR(50)
+);
+
+CREATE TABLE estimate (
+    id SERIAL PRIMARY KEY,
+    customer_id INTEGER REFERENCES customer(id),
+    estimate_date DATE,
+    expiry_date DATE
+); 
+
+CREATE TABLE estimate_item (
+    id SERIAL PRIMARY KEY,
+    estimate_id INTEGER REFERENCES estimate(id),
+    item_id INTEGER REFERENCES item(id),
+    quantity INTEGER,
+    unit_price NUMERIC(10,2),
+    gst NUMERIC(5,2),
+    discount NUMERIC(5,2),
+    total_amount NUMERIC(12,2)
+);
+
+
+
+CREATE TABLE invoice (
+    id SERIAL PRIMARY KEY,
+    customer_id INTEGER REFERENCES customer(id),
+    invoice_number VARCHAR(20),
+    payment_method VARCHAR(20),  -- Cash, UPI, Cheque, etc.
+    payment_terms VARCHAR(10),   -- Net15, Net30, etc.
+    due_date DATE
+);
+
+CREATE TABLE invoice_item (
+    id SERIAL PRIMARY KEY,
+    invoice_id INTEGER REFERENCES invoice(id),
+    item_id INTEGER REFERENCES item(id),
+    quantity INTEGER,
+    unit_price NUMERIC(10,2),
+    gst NUMERIC(5,2),
+    discount NUMERIC(5,2),
+    total_amount NUMERIC(12,2)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+ 
+
