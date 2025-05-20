52ad018551651a3cb30e89de8c597fb87a081467
Ajaykumar Wade <92783730+AjayKumar049@users.noreply.github.com>
2025-05-20 12:49:36 +0530

    Update SecurityConfig.java

--- BillingSystem/src/main/java/com/example/billing/securityconfig/SecurityConfig.java
+++ BillingSystem/src/main/java/com/example/billing/securityconfig/SecurityConfig.java
@@ -1,37 +1,38 @@
 package com.example.billing.securityconfig;
+
+
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import com.example.billing.service.AuthenticationService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-	private AuthenticationService authenticationService;
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-	
-	  @Bean 
-	  public BCryptPasswordEncoder passwordEncoder() { 
-		  return new
-	 	BCryptPasswordEncoder(); 
-	  }
-	 
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
 
-	  @Bean
-	  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-	      http.csrf(csrf -> csrf.disable()) 
-	          .authorizeHttpRequests(auth -> auth
-	              .requestMatchers("/auth/SignUp", "/auth/Signin").permitAll() 
-	              .anyRequest().authenticated() // secure all other requests
-	          );
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable()).formLogin(form -> form.disable()).authorizeHttpRequests(
+				auth -> auth.requestMatchers("/auth/SignUp", "/auth/Signin").permitAll().anyRequest().authenticated());
 
-	      return http.build();
-	  }
-
+		return http.build();
+	}
 }
+
 
 
