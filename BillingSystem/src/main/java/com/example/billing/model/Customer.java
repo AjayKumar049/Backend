5d585b6c0ea9028806295a33fc830de385808b43
Ajaykumar Wade <92783730+AjayKumar049@users.noreply.github.com>
2025-05-24 12:41:54 +0530

    Update Customer.java

--- BillingSystem/src/main/java/com/example/billing/model/Customer.java
+++ BillingSystem/src/main/java/com/example/billing/model/Customer.java
@@ -3,170 +3,245 @@ package com.example.billing.model;
 import jakarta.validation.constraints.NotNull;
 
 public class Customer {
+    
+    @NotNull(message="CustomerId is required")
+    private Integer customerId;
 
-    private int customerId;
+    @NotNull(message = "Type is required")
+    private String type;           // Business or Individual
 
-    @NotNull(message = "FirstName is required")
+    @NotNull(message = "Salutation is required")
+    private String salutation;
+
+    @NotNull(message = "First Name is required")
     private String firstName;
 
-    @NotNull(message = "LastName is required")
+    @NotNull(message = "Last Name is required")
     private String lastName;
 
-    @NotNull(message = "Company is required")
-    private String company;
+    @NotNull(message = "Company Name is required")
+    private String companyName;
 
     @NotNull(message = "Email is required")
     private String email;
 
-    @NotNull(message = "GST number is required")
-    private String gstNumber;
+    @NotNull(message = "GSTIN is required")
+    private String gstin;
 
-    @NotNull(message = "Phonenumber is required")
-    private String phoneNumber;
-    
-    @NotNull(message = "type is required")
-    private String type;
-    
-    @NotNull(message = "Salutation is required")
-    private String salutation;
+    @NotNull(message = "Phone is required")
+    private String phone;
 
+    @NotNull(message = "Attention is required")
+    private String attention;
+
+    @NotNull(message = "Shipping Address is required")
+    private String shippingAddress;
+
+    @NotNull(message = "City is required")
+    private String city;
+
+    @NotNull(message = "District is required")
+    private String district;
+
+    @NotNull(message = "State is required")
+    private String state;
+
+    @NotNull(message = "Country is required")
+    private String country;
+    
     public Customer() {
+        
     }
+
 
     private Customer(Builder builder) {
         this.customerId = builder.customerId;
-        this.firstName = builder.firstName;
-        this.lastName = builder.lastName;
-        this.company = builder.company;
-        this.email = builder.email;
-        this.gstNumber = builder.gstNumber;
-        this.phoneNumber = builder.phoneNumber;
         this.type = builder.type;
         this.salutation = builder.salutation;
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.companyName = builder.companyName;
+        this.email = builder.email;
+        this.gstin = builder.gstin;
+        this.phone = builder.phone;
+        this.attention = builder.attention;
+        this.shippingAddress = builder.shippingAddress;
+        this.city = builder.city;
+        this.district = builder.district;
+        this.state = builder.state;
+        this.country = builder.country;
     }
 
-    // Getters and Setters
+    
+    public Integer getCustomerId() {
+		return customerId;
+	}
 
-    public int getCustomerId() {
-        return customerId;
-    }
 
-    public void setCustomerId(int customerId) {
-        this.customerId = customerId;
-    }
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public String getType() {
+		return type;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public void setType(String type) {
+		this.type = type;
+	}
 
-    public String getCompany() {
-        return company;
-    }
 
-    public void setCompany(String company) {
-        this.company = company;
-    }
+	public String getSalutation() {
+		return salutation;
+	}
 
-    public String getEmail() {
-        return email;
-    }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setSalutation(String salutation) {
+		this.salutation = salutation;
+	}
 
-    public String getGstNumber() {
-        return gstNumber;
-    }
 
-    public void setGstNumber(String gstNumber) {
-        this.gstNumber = gstNumber;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public String getType() {
-        return type;
-    }
 
-    public void setType(String type) {
-        this.type = type;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public String getSalutation() {
-        return salutation;
-    }
 
-    public void setSalutation(String salutation) {
-        this.salutation = salutation;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    // Builder
-    public static Builder builder() {
-        return new Builder();
-    }
 
-    public static class Builder {
-        private int customerId;
-        private String firstName;
-        private String lastName;
-        private String company;
-        private String email;
-        private String gstNumber;
-        private String phoneNumber;
+	public String getCompanyName() {
+		return companyName;
+	}
+
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getGstin() {
+		return gstin;
+	}
+
+
+	public void setGstin(String gstin) {
+		this.gstin = gstin;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	public String getAttention() {
+		return attention;
+	}
+
+
+	public void setAttention(String attention) {
+		this.attention = attention;
+	}
+
+
+	public String getShippingAddress() {
+		return shippingAddress;
+	}
+
+
+	public void setShippingAddress(String shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	public String getDistrict() {
+		return district;
+	}
+
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+
+	public String getState() {
+		return state;
+	}
+
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+	public static class Builder {
+        private Integer customerId;
         private String type;
         private String salutation;
+        private String firstName;
+        private String lastName;
+        private String companyName;
+        private String email;
+        private String gstin;
+        private String phone;
+        private String attention;
+        private String shippingAddress;
+        private String city;
+        private String district;
+        private String state;
+        private String country;
 
-        public Builder customerId(int customerId) {
+        public Builder customerId(Integer customerId) {
             this.customerId = customerId;
-            return this;
-        }
-
-        public Builder firstName(String firstName) {
-            this.firstName = firstName;
-            return this;
-        }
-
-        public Builder lastName(String lastName) {
-            this.lastName = lastName;
-            return this;
-        }
-
-        public Builder company(String company) {
-            this.company = company;
-            return this;
-        }
-
-        public Builder email(String email) {
-            this.email = email;
-            return this;
-        }
-
-        public Builder gstNumber(String gstNumber) {
-            this.gstNumber = gstNumber;
-            return this;
-        }
-
-        public Builder phoneNumber(String phoneNumber) {
-            this.phoneNumber = phoneNumber;
             return this;
         }
 
@@ -180,8 +255,71 @@ public class Customer {
             return this;
         }
 
+        public Builder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder companyName(String companyName) {
+            this.companyName = companyName;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder gstin(String gstin) {
+            this.gstin = gstin;
+            return this;
+        }
+
+        public Builder phone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder attention(String attention) {
+            this.attention = attention;
+            return this;
+        }
+
+        public Builder shippingAddress(String shippingAddress) {
+            this.shippingAddress = shippingAddress;
+            return this;
+        }
+
+        public Builder city(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public Builder district(String district) {
+            this.district = district;
+            return this;
+        }
+
+        public Builder state(String state) {
+            this.state = state;
+            return this;
+        }
+
+        public Builder country(String country) {
+            this.country = country;
+            return this;
+        }
+
         public Customer build() {
             return new Customer(this);
         }
     }
+
+
+	
 }
